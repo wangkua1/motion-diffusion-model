8df320a71ad314d7bb3f0fded9c486fc3382fa14
Jackson Wang <kcjacksonwang@gmail.com>
2023-02-19 18:04:06 -0800

    .

--- utils/parser_util.py
+++ utils/parser_util.py
@@ -232,6 +232,7 @@ def add_emp_options(parser):
     group = parser.add_argument_group('emp')
     group.add_argument("--data_config_path", required=True, type=str)
     group.add_argument("--db", default=0, type=int)
+    group.add_argument("--eval_hmr_every", default=5, type=int)
 
 
 
