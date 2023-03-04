300b0cabae60ee2dff74abeccbfbdf868add4596
Jackson Wang <kcjacksonwang@gmail.com>
2023-03-03 18:25:08 -0800

    .

--- utils/parser_util.py
+++ utils/parser_util.py
@@ -250,6 +250,7 @@ def add_emp_options(parser):
     group.add_argument("--e_pose_loss_weight", default=60, type=int)
     group.add_argument("--e_shape_loss_weight", default=60, type=int)
     group.add_argument("--db_n_batches", default=-1, type=int, help='A debugging option for evaluation script.  Use only n evaluation batches.')
+    group.add_argument("--baseline_name", default='', type=str, help='Used in `20230303_evaluate_baseline.py`.')
     
 def train_emp_args():
     parser = ArgumentParser()
